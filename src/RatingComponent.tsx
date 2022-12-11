a5dfdbc1eb1607d5214fd7e128c403c486862e5e
Matej <matejchalachan@gmail.com>
2022-12-11 21:08:52 +0100

    changed from type to interface

--- src/RatingComponent.tsx
+++ src/RatingComponent.tsx
@@ -1,32 +1,33 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './RatingComponent.css'
 import iconStar from './assets/icon-star.svg'
 
-type RatingComponentProps = {
+interface RatingComponentProps {
     handleSubmit: Function;
-};
+}
 
 function RatingComponent({handleSubmit}: RatingComponentProps) {
     const [rating, setRating] = useState<number>(0)
 
-    const availableRatings: Array<number> = [1,2,3,4,5]
+    const availableRatings: number[] = [1, 2, 3, 4, 5]
 
     return (
         <div className="rating-container">
-            <div className={'star-icon'}><img src={iconStar} /></div>
+            <div className={'star-icon'}><img src={iconStar}/></div>
             <h1>How did we do?</h1>
-            <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+            <p>Please let us know how we did with your support request. All feedback is appreciated to help us improve
+                our offering!</p>
 
             <div className="rating-numbers-container">
                 {availableRatings.map((number) => (
-                        <div className={rating === number ? 'checked' : ''} onClick={() => setRating(number)} key={number}>
-                            {number}
-                        </div>
-                    ))}
+                    <div className={rating === number ? 'checked' : ''} onClick={() => setRating(number)} key={number}>
+                        {number}
+                    </div>
+                ))}
             </div>
 
             <div>
-                <button onClick={() => handleSubmit(true,rating)} className={'rating-button'}>Submit</button>
+                <button onClick={() => handleSubmit(true, rating)} className={'rating-button'}>Submit</button>
             </div>
         </div>
     )
